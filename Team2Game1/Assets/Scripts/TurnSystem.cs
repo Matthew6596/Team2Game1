53fea925c4023b9a6782cb0394848aad138f0e22
Matthew6596 <114946013+Matthew6596@users.noreply.github.com>
2024-04-02 18:48:54 -0400

    Added bear/player energy

--- Team2Game1/Assets/Scripts/TurnSystem.cs
+++ Team2Game1/Assets/Scripts/TurnSystem.cs
@@ -10,13 +10,22 @@ public class TurnSystem : MonoBehaviour
     static public int PlayerTurnsLeft=3;
 
     public TMP_Text RoundText;
+    public TMP_Text PlayerHpText;
+    public TMP_Text BearHpText;
     static TMP_Text roundText;
+    static TMP_Text playerText;
+    static TMP_Text bearText;
+
+    static public int PlayerEnergy = 100;
+    static public int BearEnergy = 100;
 
     static bool bearTurn = false;
     // Start is called before the first frame update
     void Start()
     {
         roundText = RoundText;
+        playerText = PlayerHpText;
+        bearText = BearHpText;
         BeginRound();
     }
 
@@ -40,6 +49,20 @@ public class TurnSystem : MonoBehaviour
 
         //
         bearTurn = false;
+        playerText.text = PlayerEnergy.ToString();
         BeginRound();
+    }
+
+    static public void BearGotHugged(int hugPow)
+    {
+        BearEnergy -= hugPow;
+
+        if (BearEnergy <= 0)
+        {
+            BearEnergy = 0;
+            //Player wins!
+        }
+
+        bearText.text = BearEnergy.ToString();
     }
 }
